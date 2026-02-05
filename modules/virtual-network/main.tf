bb54280dfafcce9281d22cada0716ffaadff48d1
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-02-05 21:55:06 +0100

    subnets now support integration for private endpoints (#3)

--- modules/virtual-network/main.tf
+++ modules/virtual-network/main.tf
@@ -13,11 +13,12 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_subnet" "subnets" {
   for_each = var.subnets
 
-  name                 = each.key
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = each.value.address_prefixes
-  service_endpoints    = each.value.service_endpoints
+  name                              = each.key
+  resource_group_name               = azurerm_resource_group.rg.name
+  virtual_network_name              = azurerm_virtual_network.main.name
+  address_prefixes                  = each.value.address_prefixes
+  service_endpoints                 = each.value.service_endpoints
+  private_endpoint_network_policies = each.value.private_endpoint_network_policies != null ? each.value.private_endpoint_network_policies : "Disabled"
 
   dynamic "delegation" {
     for_each = each.value.delegation != null ? [each.value.delegation] : []
