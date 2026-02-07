ab9fd378f26829546a6b1ee4dc4022ff74b85333
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-02-07 13:07:15 +0100

    container registry now outputs login server as url (#7)

--- modules/container-registry/outputs.tf
+++ modules/container-registry/outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   description = "The name of the container registry"
   value       = azurerm_container_registry.current.name
 }
+
+output "url" {
+  description = "The url of the container registry"
+  value       = azurerm_container_registry.current.login_server
+}
