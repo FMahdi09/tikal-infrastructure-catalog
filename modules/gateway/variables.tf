f18aa8c35a735ceffd21afab5ac96ed7f5bc12fd
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-02-11 21:08:26 +0100

    added gateway module (#12)

---
+++ modules/gateway/variables.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  description = "The region where the gateway will be deployed"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment where the gateway will be deployed"
+  type        = string
+}
+
+variable "public_ip_id" {
+  description = "The id of the public ip used by the gateway"
+  type        = string
+}
+
+variable "key_vault_id" {
+  description = "The id of the keyvault which contains the needed certificates"
+  type        = string
+}
+
+variable "key_vault_secret_id" {
+  description = "The id of the keyvault certificate which should be used"
+  type        = string
+}
+
+variable "listeners" {
+  description = "Map of listeners of the gateway"
+  type = map(object({
+    hostname = string
+    fqdn     = string
+    priority = number
+  }))
+}
