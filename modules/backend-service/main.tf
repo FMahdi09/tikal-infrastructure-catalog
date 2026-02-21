9badf7ea3be412d35ece29ac2b5f52cfdd2fef0b
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-02-21 20:02:00 +0100

    feat: backend service now creates its own keyvault (#20)

--- modules/backend-service/main.tf
+++ modules/backend-service/main.tf
@@ -85,3 +85,137 @@ resource "azurerm_postgresql_flexible_server_database" "database" {
     prevent_destroy = false
   }
 }
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "current" {
+  name                        = "tikal-${var.name}-keyvault"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "identity" {
+  key_vault_id = azurerm_key_vault.current.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_user_assigned_identity.identity.principal_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+  ]
+}
+
+resource "azurerm_key_vault_secret" "db_password" {
+  key_vault_id = azurerm_key_vault.current.id
+  name         = "Database--Password"
+  value        = var.database_credentials.password
+
+  depends_on = [azurerm_key_vault_access_policy.client-config]
+}
+
+resource "azurerm_key_vault_secret" "db_username" {
+  key_vault_id = azurerm_key_vault.current.id
+  name         = "Database--Username"
+  value        = var.database_credentials.user
+
+  depends_on = [azurerm_key_vault_access_policy.client-config]
+}
+
+resource "azurerm_key_vault_secret" "db_port" {
+  key_vault_id = azurerm_key_vault.current.id
+  name         = "Database--Port"
+  value        = 5432
+
+  depends_on = [azurerm_key_vault_access_policy.client-config]
+}
+
+resource "azurerm_key_vault_secret" "db_host" {
+  key_vault_id = azurerm_key_vault.current.id
+  name         = "Database--Host"
+  value        = data.azurerm_postgresql_flexible_server.postgres.fqdn
+
+  depends_on = [azurerm_key_vault_access_policy.client-config]
+}
+
+resource "azurerm_key_vault_secret" "identity_db_name" {
+  key_vault_id = azurerm_key_vault.current.id
+  name         = "Database--DatabaseName"
+  value        = "${var.name}-db"
+
+  depends_on = [azurerm_key_vault_access_policy.client-config]
+}
+
+resource "azurerm_key_vault_access_policy" "client-config" {
+  key_vault_id = azurerm_key_vault.current.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  certificate_permissions = [
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "Purge",
+    "SetIssuers",
+    "Update",
+  ]
+
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey",
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set",
+  ]
+
+  storage_permissions = [
+    "Backup",
+    "Delete",
+    "DeleteSAS",
+    "Get",
+    "GetSAS",
+    "List",
+    "ListSAS",
+    "Purge",
+    "Recover",
+    "RegenerateKey",
+    "Restore",
+    "Set",
+    "SetSAS",
+    "Update"
+  ]
+}
