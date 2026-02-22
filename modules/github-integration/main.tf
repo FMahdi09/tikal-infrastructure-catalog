f91ecb85758361a85d1a7ab7eb4e7fa2820cecd8
FMahdi09 <117906091+FMahdi09@users.noreply.github.com>
2026-02-22 09:11:23 +0100

    feat: added github integration module (#21)

---
+++ modules/github-integration/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.environment}-${var.region}-github-rg"
+  location = var.region
+}
+
+resource "azapi_resource" "github_network_settings" {
+  type                      = "GitHub.Network/networkSettings@2024-04-02"
+  name                      = "github-network-settings"
+  location                  = azurerm_resource_group.rg.location
+  parent_id                 = azurerm_resource_group.rg.id
+  schema_validation_enabled = false
+  body = {
+    properties = {
+      businessId = var.github_database_id
+      subnetId   = var.subnet_id
+    }
+  }
+  response_export_values = ["tags.GitHubId"]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
---
+++ modules/github-integration/outputs.tf
@@ -0,0 +1,4 @@
+output "github_network_settings_id" {
+  description = "ID of the GitHub.Network/networkSettings resource"
+  value       = jsondecode(azapi_resource.github_network_settings.output).tags.GitHubId
+}
