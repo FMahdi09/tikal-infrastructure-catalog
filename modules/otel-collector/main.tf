76264466d982e590cb89bd63b8871908653feb4c
Fabian Mahdi <Fabian@Mahdi.at>
2026-03-03 17:39:57 +0000

    feat: refactored otel collector module to use container app instead of group

--- modules/otel-collector/main.tf
+++ modules/otel-collector/main.tf
@@ -25,47 +25,10 @@ resource "azurerm_storage_share_file" "otel_config_file" {
   source            = "${path.module}/config.yaml"
 }
 
-resource "azurerm_container_group" "otel-containergroup" {
-  name                = "${var.environment}-${var.region}-otel-container-group"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "Private"
-  os_type             = "Linux"
-
-  container {
-    name  = "otel-collector"
-    image = "otel/opentelemetry-collector-contrib"
-
-    cpu    = "0.2"
-    memory = "0.5"
-
-    ports {
-      port     = 4317
-      protocol = "TCP"
-    }
-    ports {
-      port     = 4318
-      protocol = "TCP"
-    }
-    ports {
-      port     = 13133
-      protocol = "TCP"
-    }
-
-    environment_variables = {
-      "GRAFANA_CLOUD_INSTANCE_ID"   = var.grafana_cloud_instance_id,
-      "GRAFANA_CLOUD_API_KEY"       = var.grafana_cloud_api_key,
-      "GRAFANA_CLOUD_OTLP_ENDPOINT" = var.grafana_cloud_otlp_endpoint,
-    }
-
-    volume {
-      name                 = "config"
-      mount_path           = "/etc/otelcol-contrib"
-      storage_account_name = azurerm_storage_account.this.name
-      storage_account_key  = azurerm_storage_account.this.primary_access_key
-      share_name           = azurerm_storage_share.this.name
-    }
-  }
-
-  subnet_ids = [var.subnet_id]
+resource "azurerm_container_app_environment" "this" {
+  name                           = "${var.environment}-${var.region}-otel-container-env"
+  location                       = azurerm_resource_group.rg.location
+  resource_group_name            = azurerm_resource_group.rg.name
+  infrastructure_subnet_id       = var.subnet_id
+  internal_load_balancer_enabled = true
 }
--- modules/otel-collector/outputs.tf
+++ modules/otel-collector/outputs.tf
@@ -1,4 +1,4 @@
 output "ip_address" {
   description = "The private ip address of the otel collector"
-  value       = azurerm_container_group.otel-containergroup.ip_address
+  value       = azurerm_container_app_environment.this.static_ip_address
 }
